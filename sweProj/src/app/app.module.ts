71174f489f682b5614b96b5f474f982d217839a3
danijel.glisovic <glisovic.danijel96@gmail.com>
2018-07-14 22:04:22 +0200

    en route

--- sweProj/src/app/app.module.ts
+++ sweProj/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LogInComponentComponent } from './Components/log-in-component/log-in-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponentComponent
   ],
   imports: [
     BrowserModule
